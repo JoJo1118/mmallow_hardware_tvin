047a240bdab96d2a2295bf1814012d5641ee5fc4
lianghu.su <lianghu.su@amlogic.com>
2014-03-28 15:12:40 +0800

    pd[88786]: Fix when open hdmiin, seek action in local movie player cause crash error. modified by Kong Dezhi.

--- vdin/vdin_canvas.c
+++ vdin/vdin_canvas.c
@@ -26,19 +26,15 @@ MODULE_PARM_DESC(max_buf_num, "vdin max buf num.\n");
 
 const unsigned int vdin_canvas_ids[2][VDIN_CANVAS_MAX_CNT] = {
 	{
-		12, 13, 14, 15, 16, 17,
-		18, 19, 20, 21, 22, 23,
 		24, 25, 26, 27, 28, 29,
 		30, 31, 32, 33, 34, 35,
-
+		36, 37, 38, 39, 40, 41,
 	},
 	{
-		36, 37, 38, 39, 40, 41,
 		42, 43, 44, 45, 46, 47,
 		48, 49, 50, 51, 52, 53,
 		54, 55, 56, 57, 58, 59,
 	},
-
 };
 
 inline void vdin_canvas_init(struct vdin_dev_s *devp)
--- vdin/vdin_canvas.h
+++ vdin/vdin_canvas.h
@@ -21,7 +21,7 @@
 
 #define VDIN_CANVAS_MAX_WIDTH			1920
 #define VDIN_CANVAS_MAX_HEIGH			2228
-#define VDIN_CANVAS_MAX_CNT			24
+#define VDIN_CANVAS_MAX_CNT			18
 
 extern const unsigned int vdin_canvas_ids[2][VDIN_CANVAS_MAX_CNT];
 extern void vdin_canvas_init(struct vdin_dev_s *devp);

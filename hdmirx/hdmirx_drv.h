89f806102f6f5b41523f5b7916e19fd3d5b4a824
MingLiang Dong <mingliang.dong@amlogic.com>
2015-08-17 19:01:42 +0800

    PD#11137: hdmirx: modify edid best resolution
    Change-Id: I68015098afe5e9c1e354b573d25367c819c04c05

--- hdmirx/hdmirx_drv.h
+++ hdmirx/hdmirx_drv.h
@@ -19,7 +19,7 @@
 #include "../tvin_global.h"
 #include "../tvin_format_table.h"
 
-#define HDMIRX_VER "Ref.2015/07/28"
+#define HDMIRX_VER "Ref.2015/08/17"
 
 #define HDMI_STATE_CHECK_FREQ     (20*5)
 #define ABS(x) ((x)<0 ? -(x) : (x))
